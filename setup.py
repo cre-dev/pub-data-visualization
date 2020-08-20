57e3921fad9a85caa757def4461d5ff6ecae5433
cre-os <opensource@cre.fr>
2020-08-20 13:27:30 +0200

    rename scripts/

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+from distutils.core import setup
+
+setup(
+      name             = 'energy-data-visualization',
+      version          = '0.1.dev0',
+      packages         = setuptools.find_packages(),
+      scripts          = [],
+      author           = 'CRE',
+      author_email     = 'opensource[at]cre.fr',
+      license          = 'MIT License',
+      long_description = open('README.md').read(),
+      python_requires  = ">= 3.8",
+      install_requires = [
+                          'matplotlib',
+                          'numpy',
+                          'pandas',
+                          'seaborn',
+			  'termcolor',
+                          ],
+      )
