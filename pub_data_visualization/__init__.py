e087b04446575c1da24993a89f13db518cf2add9
cre-os <opensource@cre.fr>
2020-08-26 15:17:26 +0200

    updated documenbtation of the root module

--- pub_data_visualization/__init__.py
+++ pub_data_visualization/__init__.py
@@ -4,6 +4,30 @@ Visualization module for public energy data with Python
 =======================================================
 
 See https://github.com/cre-os/energy-data-visualization for a presentation of this project.
+
+All the subpackages have a similar structure : 
+```
+TypeOfData/
+├── __init__.py
+├── load/
+│  ├── __init__
+│  ├── load.py
+│  ├── dataSource1/
+│  ├── dataSource2/
+│  ├── ...
+├── plot/
+│  ├── __init__.py
+│  ├── plotFunction1.py
+│  ├── plotFunction2.py
+│  ├── subplot/
+│  ├── ...
+├── tools/
+│  ├── __init__.py
+│  ├── tool1.py
+│  ├── tool2.py
+│  ├── ...
+```
+
 """
 
 
