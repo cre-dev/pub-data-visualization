f34a6ec2fbdc72333408ffc365e2e1b914488e31
cre-os <opensource@cre.fr>
2020-10-01 15:38:30 +0200

    added cascade_frequencies.py

---
+++ pub_data_visualization/global_var/cascade_frequencies.py
@@ -0,0 +1,27 @@
+
+"""
+    Definition of the cascade effect between different WEPs.
+"""
+
+from . import user_defined_names as global_var
+
+cascade_frequencies = [global_var.contract_frequency_year,
+                       global_var.contract_frequency_boy,
+                       global_var.contract_frequency_season,
+                       global_var.contract_frequency_bos,
+                       global_var.contract_frequency_quarter,
+                       global_var.contract_frequency_boq,
+                       global_var.contract_frequency_month,
+                       global_var.contract_frequency_bom,
+                       global_var.contract_frequency_week,
+                       global_var.contract_frequency_bow,
+                       global_var.contract_frequency_week,
+                       global_var.contract_frequency_weekend,
+                       global_var.contract_frequency_days,
+                       global_var.contract_frequency_day,
+                       global_var.contract_frequency_bloc,
+                       global_var.contract_frequency_hour,
+                       ]
+
+
+
