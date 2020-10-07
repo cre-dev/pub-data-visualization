b745210ff19c1990bb3a1cd9dab8e1e56b09355b
cre-os <opensource@cre.fr>
2020-10-07 10:19:55 +0200

    changed company_name to producer_name for clarity

--- pub_data_visualization/outages/load/rte/transcode/producer_name.py
+++ pub_data_visualization/outages/load/rte/transcode/producer_name.py
@@ -9,16 +9,16 @@ import numpy as np
 from ..... import global_var
 
 
-company_name = {'EDF'                     : global_var.producer_name_edf, 
-                'ENGIE'                   : global_var.producer_name_engie, 
-                'UNIPER'                  : global_var.producer_name_eon, 
-                'GDF'                     : global_var.producer_name_engie, 
-                'PSS POWER'               : global_var.producer_name_pss,
-                'GAZEL ENERGIE'           : global_var.producer_name_gazel,
-                'CELEST POWER'            : global_var.producer_name_gazel, 
-                'ALPIQ'                   : global_var.producer_name_alpiq, 
-                'TOTAL RAFFINAGE FRANCE ' : global_var.producer_name_total,
-                'DIRECT ENERGIE'          : global_var.producer_name_total, 
-                'TOTAL DIRECT ENERGIE'    : global_var.producer_name_total,
-                np.nan                    : global_var.producer_name_unknown,
-                }
+producer_name = {'EDF'                     : global_var.producer_name_edf, 
+                 'ENGIE'                   : global_var.producer_name_engie, 
+                 'UNIPER'                  : global_var.producer_name_eon, 
+                 'GDF'                     : global_var.producer_name_engie, 
+                 'PSS POWER'               : global_var.producer_name_pss,
+                 'GAZEL ENERGIE'           : global_var.producer_name_gazel,
+                 'CELEST POWER'            : global_var.producer_name_gazel, 
+                 'ALPIQ'                   : global_var.producer_name_alpiq, 
+                 'TOTAL RAFFINAGE FRANCE ' : global_var.producer_name_total,
+                 'DIRECT ENERGIE'          : global_var.producer_name_total, 
+                 'TOTAL DIRECT ENERGIE'    : global_var.producer_name_total,
+                 np.nan                    : global_var.producer_name_unknown,
+                 }
