fcd670ab4e2ed35191af0a063740775e6c1ad4de
cre-os <opensource@cre.fr>
2021-06-07 13:29:46 +0200

    added default dates

--- pub_scripts/weather/main_distribution.py
+++ pub_scripts/weather/main_distribution.py
@@ -6,6 +6,7 @@ The script allows the user to draw the empirical boxplots
 of the weather conditions for the different months and different years.
 """
 
+import pandas as pd
 #
 from pub_data_visualization import global_var, weather
 
@@ -13,6 +14,8 @@ from pub_data_visualization import global_var, weather
 data_source_weather = global_var.data_source_meteofrance
 weather_nature      = global_var.weather_nature_observation
 weather_quantity    = global_var.weather_temperature_celsius
+date_min            = pd.Timestamp('2016').tz_localize('CET')
+date_max            = pd.Timestamp('2021').tz_localize('CET')
 ###############################################################################
 figsize    = global_var.figsize_horizontal_ppt
 folder_out = global_var.path_plots
@@ -20,7 +23,10 @@ close      = False
 ###############################################################################
 
 ### Load
-df = weather.load(source = data_source_weather)
+df = weather.load(source   = data_source_weather,
+				  date_min = date_min,
+				  date_max = date_max,
+				  )
 
 ### Plot
 weather.plot.distribution(df, 
