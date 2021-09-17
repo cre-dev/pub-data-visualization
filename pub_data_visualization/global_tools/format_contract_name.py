1b142a0157b49919bc0342286cbc0682a89fde65
cre-os <opensource@cre.fr>
2021-09-17 12:12:13 +0200

    update user defined names

--- pub_data_visualization/global_tools/format_contract_name.py
+++ pub_data_visualization/global_tools/format_contract_name.py
@@ -3,7 +3,7 @@
 from .. import global_var
 
 def format_contract_name(year,
-                         product,
+                         frequency,
                          delivery_period,
                          profile   = None,
                          map_code  = None,
@@ -13,11 +13,11 @@ def format_contract_name(year,
         Returns a generic name for a given contract.
  
         :param year: The year of the delivery
-        :param product: The type of delivery contract (year, month, etc.)
+        :param frequency: The type of delivery contract (year, month, etc.)
         :param delivery_period: The index of the delivery contract
         :param profile: profile of the delivery contract
         :type year: int
-        :type product: string
+        :type frequency: string
         :type delivery_periodx: int
         :type profile: string
         :return: The formatted name of the contract
@@ -25,7 +25,7 @@ def format_contract_name(year,
     """
     assert len(str(year)) == 4
     assert str(year).isdigit()
-    assert type(product) == str
+    assert type(frequency) == str
     assert profile in {global_var.contract_profile_base,
                        global_var.contract_profile_ofpk,
                        global_var.contract_profile_peak,
@@ -33,12 +33,19 @@ def format_contract_name(year,
                        global_var.contract_profile_half_hour,
                        None,
                        }
-    return '.'.join(filter(None, ['{year}{product}{delivery_period}'.format(year            = year,
-                                                                            product         = product,
-                                                                            delivery_period = delivery_period,
-                                                                            ),
-                                  profile,
-                                  map_code,
+    return '.'.join(filter(None, [map_code,
                                   commodity,
+                                  '{year}{frequency}{delivery_period}'.format(year            = year,
+                                                                              frequency       = frequency,
+                                                                              delivery_period = delivery_period,
+                                                                              ),
+                                  (profile
+                                   if profile not in [global_var.contract_profile_hour,
+                                                      global_var.contract_profile_half_hour,
+                                                      global_var.contract_profile_bloc,
+                                                      ]
+                                   else
+                                   None
+                                   ),
                                   ]))
     
--- pub_data_visualization/production/load/rte/transcode/format_str_date.py
+++ pub_data_visualization/production/load/rte/transcode/format_str_date.py
@@ -25,11 +25,19 @@ def format_str_date(str_dates):
                            hour   = int(dt_match.group(4)),
                            minute = int(dt_match.group(5)),
                            )
+    dt_end = dt.datetime(year   = int(dt_match.group(3)),
+                         month  = int(dt_match.group(2)),
+                         day    = int(dt_match.group(1)),
+                         hour   = int(dt_match.group(6)) % 24,
+                         minute = int(dt_match.group(7)),
+                         )
     # Check
     assert (int(dt_match.group(6)) - int(dt_match.group(4))) % 24 == 1
     assert int(dt_match.group(5)) == 0
     assert int(dt_match.group(7)) == 0
     dt_begin = pd.Timestamp(dt_begin)
+    dt_end   = pd.Timestamp(dt_end)
+    dt_mean  = dt_begin + (dt_end - dt_begin)/2
     # Format
     if global_tools.dt_exists_in_tz(dt_begin, 'CET'):
         dt_begin = pd.to_datetime(dt_begin).tz_localize('CET', ambiguous = True)
